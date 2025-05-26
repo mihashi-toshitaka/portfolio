d882212ae325c85062f61ccfd5d1e111ef556988
mihashi-toshitaka <mihashi.work.address@gmail.com>
2025-05-27 00:14:45 +0900

    first commit

---
+++ src/test/java/site/mihashi_toshitaka/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package site.mihashi_toshitaka.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
