d381c85474bb5a9a4cf391fd1f0fdb44711d257f
mihashi-toshitaka <mihashi.work.address@gmail.com>
2025-05-27 21:10:56 +0900

    feat: ドメインの変更

--- src/main/java/com/mihashi_toshitaka/portfolio/PortfolioApplication.java
+++ src/main/java/com/mihashi_toshitaka/portfolio/PortfolioApplication.java
@@ -1,4 +1,4 @@
-package site.mihashi_toshitaka.portfolio;
+package com.mihashi_toshitaka.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
