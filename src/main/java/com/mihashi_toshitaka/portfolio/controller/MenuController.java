eaaa9f106da6e7f692d540b1cffbfeac54cdabe2
mihashi-toshitaka <mihashi.work.address@gmail.com>
2025-06-01 22:04:42 +0900

    feat: redissessionの実装

--- src/main/java/com/mihashi_toshitaka/portfolio/controller/MenuController.java
+++ src/main/java/com/mihashi_toshitaka/portfolio/controller/MenuController.java
@@ -1,16 +1,29 @@
 package com.mihashi_toshitaka.portfolio.controller;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
 
     @GetMapping("")
-    public String login(Model model) {
+    public String login(HttpSession session, Model model) {
+
+        System.out.println(session.getId());
+
+        SecurityContext ctx = (SecurityContext) session.getAttribute("SPRING_SECURITY_CONTEXT");
+        if (ctx != null && ctx.getAuthentication().getPrincipal() instanceof OidcUser p) {
+            String loginId = p.getPreferredUsername();
+            System.out.println(loginId);
+        }
+
         return "menu";
     }
 
