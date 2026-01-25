e61f2473c0d1995924ada30db4484c5d4133343c
mihashi toshitaka <mihashi.work.address@gmail.com>
2026-01-25 12:07:58 +0900

    chore: 更新された依存関係と設定ファイルの修正

--- src/main/java/com/mihashi_toshitaka/portfolio/common/security/SecurityConfig.java
+++ src/main/java/com/mihashi_toshitaka/portfolio/common/security/SecurityConfig.java
@@ -1,33 +1,43 @@
 package com.mihashi_toshitaka.portfolio.common.security;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Autowired
-    private CustomSuccessHandler customSuccessHandler;
+  @Autowired private CustomSuccessHandler customSuccessHandler;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
+  @Bean
+  public SecurityFilterChain securityFilterChain(
+      HttpSecurity http, ObjectProvider<ClientRegistrationRepository> clientRegistrationRepository)
+      throws Exception {
+    http.authorizeHttpRequests(
+        auth ->
+            auth
                 // 外部向けAPIは認証不要
-                .requestMatchers("/api/public/**").permitAll()
+                .requestMatchers("/api/public/**")
+                .permitAll()
                 // プライバシーポリシーHTMLは認証不要
-                .requestMatchers("/html/privacy.html").permitAll()
+                .requestMatchers("/html/privacy.html")
+                .permitAll()
                 // ヘルスチェックエンドポイントは認証不要
-                .requestMatchers(EndpointRequest.to("health")).permitAll()
+                .requestMatchers("/actuator/health", "/actuator/health/**")
+                .permitAll()
                 // それ以外は認証が必要
-                .anyRequest().authenticated())
-                // 認証時の挙動（デフォルトログイン画面は無効）
-                .oauth2Login(
-                        oauth2 -> oauth2.loginPage("/oauth2/authorization/azure").successHandler(customSuccessHandler));
-        return http.build();
+                .anyRequest()
+                .authenticated());
+    if (clientRegistrationRepository.getIfAvailable() != null) {
+      // 認証時の挙動（デフォルトログイン画面は無効）
+      http.oauth2Login(
+          oauth2 ->
+              oauth2.loginPage("/oauth2/authorization/azure").successHandler(customSuccessHandler));
     }
-
+    return http.build();
+  }
 }
